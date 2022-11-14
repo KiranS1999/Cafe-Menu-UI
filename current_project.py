482a2ce612d746d5c12fc02f6397f64fce9285cd
Kiran Sahiba <kiransahiba9@gmail.com>
2022-11-14 16:30:10 +0000

    Added products and couriers to database

--- current_project.py
+++ current_project.py
@@ -5,30 +5,43 @@
 #Libraries
 import csv
 import operator
+import pymysql
+import os
+from dotenv import load_dotenv
+
+
+#SET UP DB CONNECTION
+# Load environment variables from .env file
+load_dotenv()
+host = os.environ.get("mysql_host")
+user = os.environ.get("mysql_user")
+password = os.environ.get("mysql_pass")
+database = os.environ.get("mysql_db")
+
+# Establish a database connection
+connection = pymysql.connect(
+    host,
+    user,
+    password,
+    database
+)
+
+cursor = connection.cursor()
+
+
 
 
 
 #create a list of products from an incoming csv file
-orders =[]
-products = [] 
-couriers = []
+orders = []
+
 
 
 
 
 
 #FUNCTION: Loading data through csv
-def load_data():
-    # LOAD products from products.csv
-    with open('products.csv', 'r') as f:
-        product_file = csv.DictReader(f)
-        for row in product_file:
-            products.append(dict(row))
-    # LOAD couriers from couriers.csv
-    with open('couriers.csv', 'r') as file:
-        courier_file = csv.DictReader(file)
-        for row in courier_file:
-            couriers.append(dict(row))
+def load_order_data():            
     # LOAD orders from orders.csv
     with open('orders.csv', 'r') as fhand:
         order_file = csv.DictReader(fhand)
@@ -36,17 +49,7 @@ def load_data():
             orders.append(dict(row))
 
 
-
-
-
-
 #FUNCTIONS: Save data to csv
-def save_product_list():
-    file = open('products.csv', 'w')
-    w = csv.DictWriter(file, fieldnames = ['Product', 'Price'])
-    w.writeheader()
-    w.writerows(products)
-    file.close()
 
 def save_order_list():
     file = open('./orders.csv', 'w')
@@ -56,13 +59,6 @@ def save_order_list():
         w.writerow(dictionary.values())
     file.close()   
 
-def save_courier_list():
-    file = open('./couriers.csv', 'w')
-    w = csv.writer(file)
-    w.writerow(['Name', 'Phone'])
-    for dictionary in couriers:
-        w.writerow(dictionary.values())
-    file.close()   
 
 
 
@@ -78,61 +74,52 @@ def save_courier_list():
 def create_product ():
     newprod = input('Please enter your new product name: ')
     newprice = input('Please enter the price: ')
-    new_prod_dict = {'Product': newprod, 'Price': newprice}
-    products.append(new_prod_dict)
-    print(products)
+    sql = "INSERT INTO products (name, price) VALUES (%s, %s)"
+    val = (newprod, newprice)
+    cursor.execute(sql, val)
+    connection.commit()
+
 
 #FUNCTION: update a product
 def update_product():
-    print ("List index-value are : ")
-            
-    for index, value in enumerate(products):
-        print(index, value)
+    cursor.execute('SELECT id, name, price FROM products') 
+    rows = cursor.fetchall()
+    for row in rows:
+        print(f'Product ID: {row[0]}, Name: {row[1]}, Price: {row[2]}')
+    prod_id = input('Index value of the product you wish to update: ')
+    prod_name = input('What is the new product name?: ') 
+    prod_price = input('What is the new price?: ')
 
-    try:
-        user_index = int(input('Index value of the product you wish to update: ')) 
-        product_to_change = products[user_index]
-        for key, value in product_to_change.items():
-            print(key, ':', value)
-        product_new = input('What is the new product name?: ')  
-        price_new = input('What is the new price?: ')
-        if product_new == '': 
-            print('Product name will not be updated')
-        else:
-            product_to_change['Product'] = product_new
+    if prod_name == '': 
+        print('Product name will not be updated')
+    else:
+        sql = "UPDATE products SET name = %s WHERE id = %s"
+        val = (prod_name, prod_id)
+        cursor.execute(sql, val)
+        connection.commit()
 
-        if price_new == '': 
-            print('Price will not be updated')
-        else:
-            product_to_change['Price'] = price_new    
+    if prod_price == '': 
+        print('Product price will not be updated')
+    else:
+        sql = "UPDATE products SET price = %s WHERE id = %s"
+        val = (prod_price, prod_id)
+        cursor.execute(sql, val)
+        connection.commit()  
 
-    except IndexError:
-        print('You have selected an unavailable index, please try again')
-        update_product()
-    except ValueError as v:
-        print('You have not entered a valid index, please try again!') 
-        update_product()
+
+
 
 #FUNCTION: delete a product
 def delete_product():
-    for index, value in enumerate(products):
-                print(index, value)
-            
-    try:
-        user_index = int(input('Please type the index value of the product you wish to delete: '))    
-        del products[user_index]
-        print(products) 
-    except IndexError as e:
-        print('You have selected an unavailable index, please try again')
-        delete_product()  
-    except ValueError as v:
-        print('You have not entered a valid index, please try again!')
-        delete_product()                 
-
-    else:
-        print('You have entered an invalid index number')
-
-
+    cursor.execute('SELECT id, name, price FROM products') 
+    rows = cursor.fetchall()
+    for row in rows:
+        print(f'Product ID: {row[0]}, Name: {row[1]}, Price: {row[2]}')
+    prod_id = input('Index value of the product you wish to delete: ')
+    sql = "DELETE FROM products WHERE id=%s"
+    val = (prod_id)
+    cursor.execute(sql, val)
+    connection.commit()  
 
 
 
@@ -144,60 +131,52 @@ def delete_product():
 
 #Function: create a new courier
 def new_courier():
-    new_courier = input('What is the name of the new courier?: ')
+    new_courier= input('What is the name of the new courier?: ')
     new_phone = input('Please enter their phone number: ')
-    new_courier_dict = {'Name': new_courier, 'Phone': new_phone}
-    couriers.append(new_courier_dict)
-    print(couriers)
+    sql = "INSERT INTO couriers (name, phone) VALUES (%s, %s)"
+    val = (new_courier, new_phone)
+    cursor.execute(sql, val)
+    connection.commit()
+    
 
 #Function: update a courier
 def update_courier():
-    print ("List index-value are : ")
-        
-    for index, value in enumerate(couriers):
-        print(index, value)
+    cursor.execute('SELECT id, name, phone FROM couriers') 
+    rows = cursor.fetchall()
+    for row in rows:
+        print(f'Courier ID: {row[0]}, Name: {row[1]}, Phone: {row[2]}')
+    courier_id = input('Index value of the product you wish to update: ')
+    courier_name = input('What is the new courier name?: ') 
+    courier_phone = input('What is the new phone number?: ')
+
+    if courier_name == '': 
+        print('Courier name will not be updated')
+    else:
+        sql = "UPDATE couriers SET name = %s WHERE id = %s"
+        val = (courier_name, courier_id)
+        cursor.execute(sql, val)
+        connection.commit()
+
+    if courier_name == '': 
+        print('Courier phone number will not be updated')
+    else:
+        sql = "UPDATE couriers SET phone = %s WHERE id = %s"
+        val = (courier_phone, courier_id)
+        cursor.execute(sql, val)
+        connection.commit()  
     
-    try:
-        user_index = int(input('Index value of the product you wish to update: ')) 
-        courier_to_change = couriers[user_index]
-        for key, value in courier_to_change.items():
-            print(key, ':', value)
-        courier_new = input('What is the new courier name?: ')  
-        phone_new = input('What is the new phone number?: ')
-        if courier_new == '': 
-            print('Name will not be updated')
-        else:
-            courier_to_change['Name'] = courier_new
-
-        if phone_new == '': 
-            print('Phone number will not be updated')
-        else:
-            courier_to_change['Phone'] = phone_new    
-
-    except IndexError:
-        print('You have selected an unavailable index, please try again')
-        update_courier()
-    except ValueError as v:
-        print('You have not entered a valid index, please try again!') 
-        update_courier()
 
 #FUNCTION: delete a courier 
 def del_courier():   
-    for index, value in enumerate(couriers):
-        print(index, value)
-    
-    try:
-        user_index = int(input('Please type the index value of the courier you wish to delete: '))    
-        del couriers[user_index]
-        print(couriers) 
-    except IndexError as e:
-        print('You have selected an unavailable index, please try again')
-            
-    except ValueError as v:
-        print('You have not entered a valid index, please try again!')
-    
-    else: #check 
-        print('You have selected an invalid index, try again')
+    cursor.execute('SELECT id, name, phone FROM couriers') 
+    rows = cursor.fetchall()
+    for row in rows:
+        print(f'Courier ID: {row[0]}, Name: {row[1]}, Phone: {row[2]}')
+    courier_id = input('Index value of the product you wish to delete: ')
+    sql = "DELETE FROM couriers WHERE id=%s"
+    val = (courier_id)
+    cursor.execute(sql, val)
+    connection.commit()  
 
 
 
@@ -239,35 +218,38 @@ def new_order():
     
 
     #add courier to the order 
-    print ("Courier list index-value are : ")
-    for index, value in enumerate(couriers):
-        print(index, value)   
-
+    cursor.execute('SELECT id, name, phone FROM couriers') 
+    rows = cursor.fetchall()
+    for row in rows:
+        print(f'Courier ID: {row[0]}, Name: {row[1]}, Phone: {row[2]}')
+    
     courier_add = input('Please type the index of the courier you would like to attach to this order: ')
     new_orders['courier_index']= courier_add 
+
+
 
     #order status to preparing
     new_orders['Order status'] = 'preparing'  
 
     #print products list with index values and get user input for what products they want
-    print ("Product list index-value are : ")
-    for index, value in enumerate(products):
-        print(index, value)
-    product_index = []
+    cursor.execute('SELECT id, name, price FROM products') 
+    rows = cursor.fetchall()
+    for row in rows:
+        print(f'Product ID: {row[0]}, Name: {row[1]}, Price: {row[2]}')
+    product_index = []    
+    
     while True:
         user_input = input('What products would you like to add to this order? Type done to exit: ')
         if user_input == 'done':
             break
         else:
             product_index.append(user_input)
+
     prod_index_str = ','.join(str(item) for item in product_index)
-    new_orders['product_index']=prod_index_str     
-
-
+    new_orders['product_index']=prod_index_str 
+    print(new_orders)    
     orders.append(new_orders)
 
-    print('The current order list:')
-    print(orders)
 
 #FUNCTION: update the order status of a product
 def update_order_status():
@@ -359,8 +341,11 @@ def del_order():
   
 def mainmenu():
     while True:
+        
+        load_order_data()
+
         print ('''Main menu
-        0. Save product, order and courier list
+        0. Save order list and exit database
         1. Products
         2. Couriers
         3. Orders''')
@@ -368,15 +353,15 @@ def mainmenu():
         user_input = int(input("Please enter menu number: "))
         
         if user_input == 0:
-            save_product_list()
-            save_courier_list()
+
             save_order_list() 
+            cursor.close()
+            connection.close()
             print('Exiting application')
             exit()   
 
         elif user_input == 1:
-            print('Product list:')
-            print(products)
+            
             print('''Product Menu:
             0. Return to main menu
             1. View products
@@ -391,7 +376,10 @@ def mainmenu():
             
             elif x == 1: 
             #view products    
-                print(products)
+                cursor.execute('SELECT * FROM products')
+                rows = cursor.fetchall()
+                for row in rows:
+                    print(f'Name: {str(row[1])}, Price: {row[2]}')
             
             elif x == 2:
             #create a new product    
@@ -423,7 +411,11 @@ def mainmenu():
                 mainmenu()
 
             elif x == 1:
-                print(couriers)
+                #View couriers
+                cursor.execute('SELECT * FROM couriers')
+                rows = cursor.fetchall()
+                for row in rows:
+                    print(f'Name: {str(row[1])}, Phone Number: {row[2]}')
 
             elif x == 2:
                 #create a new courier
@@ -479,5 +471,5 @@ def mainmenu():
         else:
                 print('You have not entered a valid menu number, please try again') 
 
-
-mainmenu()
+if __name__ == "__main__":
+    mainmenu()
